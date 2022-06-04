b7a49b6485767ccb79d54f2fbdfd30f5ac5d2106
Alex <whiplaxx@gmail.com>
2022-06-04 16:03:31 -0300

    Play sound and diff times for work and break

--- pomo.sh
+++ pomo.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/env bash
 
+work_msg="Let's work!"
+break_msg="Take a break!"
+play_sound=false
+sound_path="~/Music/bell.mp3"
+play_command="paplay --volume=35000 '${sound_path}'"
+
 arg1=$1
-shift
-args="$*"
-                                               
-min=${arg1:?Example: pomo 15 Take a break}
-sec=$((min * 60))
-msg="${args:?Example: pomo 15 Take a break}"
+arg2=$2
+
+work_min=${arg1:?Example: pomo 15 5}
+work_sec=$((min * 60))
+break_min=${arg2:?Example: pomo 15 5}
+break_sec=$((min * 60))
+
+work_sec=15
+break_sec=15
+
+echo "Pomodoro started"
+echo "\t${work_min} minutes of work"
+echo "\t${break_min} minutes of break"
 
 while true; do
-    date '+%H:%M' && sleep "${sec:?}" && notify-send -u critical -t 0 -a pomo "${msg:?}"
+    date '+%H:%M' && sleep "${work_sec:?}"
+    notify-send --expire-time=10000 --app-name=pomo "${break_msg}"
+    if $play_sound; then
+        eval $play_command
+    fi
+
+    date '+%H:%M' && sleep "${break_sec:?}"
+    notify-send --expire-time=10000 --app-name=pomo "${work_msg}"
+    if $play_sound; then
+        eval $play_command
+    fi
 done
